e60e578cfd605e4da08280a37973054985a8750e
Cheshire Cat <thacheshirecat@gmail.com>
2018-08-20 22:40:09 -0700

    Add initial file directory and code for online market place lesson.

---
+++ src/app/album-detail/album-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-detail',
+  templateUrl: './album-detail.component.html',
+  styleUrls: ['./album-detail.component.css']
+})
+export class AlbumDetailComponent
+{
+
+}
